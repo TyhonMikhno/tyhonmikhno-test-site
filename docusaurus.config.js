eab700de3d06a19c4c37a7f0ce11b57b2660cbf9
Tyhon Mikhno <timeair10000@gmail.com>
2024-05-16 00:14:45 +0300

    Added changes.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tyhon`s site',
+  tagline: 'КП-33(i love capybaras)',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
